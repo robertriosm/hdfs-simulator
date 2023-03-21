f7aa8c1d501f2de16068f5cb04db81b96ac1afab
robertriosm <87707371+robertriosm@users.noreply.github.com>
2023-03-21 14:46:18 -0600

    initial

---
+++ functions.py
@@ -0,0 +1,76 @@
+
+
+
+# lenguaje de definicion de datos
+
+def create():
+    pass
+
+def hlist():
+    pass
+
+def disable():
+    pass
+
+def isEnaled():
+    pass
+
+
+def alter():
+    pass
+
+
+def drop():
+    pass
+
+def dropAll():
+    pass
+
+def describe():
+    pass
+
+
+# lenguaje de manipulacion de datos
+
+
+def put():
+    pass
+
+
+
+def get():
+    pass
+
+
+def scan():
+    pass
+
+
+def delete():
+    pass
+
+
+
+def deleteAll():
+    pass
+
+
+def count():
+    pass
+
+
+
+def truncate():
+    pass
+
+
+
+# puntos extra
+
+
+def updateMany():
+    pass
+
+
+def InsertMany():
+    pass
---
+++ main.py
@@ -0,0 +1,15 @@
+from functions import *
+
+print('\nwelcome to fakedfs\n')
+
+op = False
+
+ye = ''
+
+
+while not op:
+    ye = input(">>> ")
+
+    if ye == 'exit':
+        op = True
+        print('adios\n')
