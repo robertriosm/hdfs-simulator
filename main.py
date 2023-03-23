19c640af0392eff5455e6996a94a5430c68acf00
andresdlRoca <andresdelaroca11@gmail.com>
2023-03-23 11:30:42 -0600

    print adios

--- main.py
+++ main.py
@@ -12,4 +12,15 @@ while not op:
 
     if ye == 'exit':
         op = True
-        print('adios\n')
+        print("""
+                                                         
+                            88 88                        
+                            88 ""                        
+                            88                           
+        ,adPPYYba,  ,adPPYb,88 88  ,adPPYba,  ,adPPYba,  
+        ""     `Y8 a8"    `Y88 88 a8"     "8a I8[    ""  
+        ,adPPPPP88 8b       88 88 8b       d8  `"Y8ba,   
+        88,    ,88 "8a,   ,d88 88 "8a,   ,a8" aa    ]8I  
+        `"8bbdP"Y8  `"8bbdP"Y8 88  `"YbbdP"'  `"YbbdP"'  
+                                                                                                   
+        """)
